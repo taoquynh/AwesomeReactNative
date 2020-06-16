e0cee2e7d6cdaaa35bada3e593a1115351a62457
Taof <taof@MacbookPro-2.local>
2020-06-16 15:27:24 +0700

    update file name

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /**
  * @format
  */
@@ -5,16 +6,15 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import HelloWorldApp from './src/screens/HelloWorld';
+import HelloWorldApp from './src/screens/1_HelloWorld';
 import Layout from './src/screens/Layout';
-import DemoTimer from './src/screens/DemoTimer';
-import TheLight from './src/screens/TurnOffTheLight';
-import Rich from './src/screens/Rich';
+import DemoTimer from './src/screens/4_DemoTimer';
+import TheLight from './src/screens/3_TurnOffTheLight';
+import Rich from './src/screens/2_Rich';
 import TimePicker from './src/screens/TimePicker';
 import DemoDateTimePicker from './src/screens/DemoDateTimePicker';
-import Bingo from './src/screens/Bingo';
+import Bingo from './src/screens/5_Bingo';
 import SomeScene from './src/screens/TestPicker';
-import PickerAndroid from './src/screens/PickerForAndroid';
-import ShowSlide from './src/screens/ShowSlide';
+import ShowSlide from './src/screens/6_ShowSlide';
 
 AppRegistry.registerComponent(appName, () => ShowSlide);
